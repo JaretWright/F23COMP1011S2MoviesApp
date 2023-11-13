989e3121d4500d4f8b8dccc2c89479c46706ca11
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-11-13 10:53:42 -0500

    Integrated API into the controller

--- src/main/java/com/example/f23comp1011s2moviesapp/SearchViewController.java
+++ src/main/java/com/example/f23comp1011s2moviesapp/SearchViewController.java
@@ -1,5 +1,6 @@
 package com.example.f23comp1011s2moviesapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -9,10 +10,12 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
+
 public class SearchViewController {
 
     @FXML
-    private ListView<?> listView;
+    private ListView<Movie> listView;
 
     @FXML
     private Label msgLabel;
@@ -36,5 +39,16 @@ public class SearchViewController {
     private void initialize()
     {
         progressBar.setVisible(false);
+    }
+
+    @FXML
+    void searchOMBD(ActionEvent event) throws IOException, InterruptedException {
+        String movieName = searchTextField.getText();
+        if (!movieName.trim().isEmpty())
+        {
+            APIResponse apiResponse = APIUtility.searchMovies(movieName.trim());
+            listView.getItems().clear();
+            listView.getItems().addAll(apiResponse.getMovies());
+        }
     }
 }
