d1b1649c52a4b4315304ab5ca260d8e1012f8cff
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-11-13 10:45:37 -0500

    Updated the controller to make the progress bar invisible

--- src/main/java/com/example/f23comp1011s2moviesapp/SearchViewController.java
+++ src/main/java/com/example/f23comp1011s2moviesapp/SearchViewController.java
@@ -1,4 +1,40 @@
 package com.example.f23comp1011s2moviesapp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class SearchViewController {
+
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    private Label msgLabel;
+
+    @FXML
+    private ImageView posterImageView;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private HBox resultsBox;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private VBox selectedVBox;
+
+    @FXML
+    private void initialize()
+    {
+        progressBar.setVisible(false);
+    }
 }
