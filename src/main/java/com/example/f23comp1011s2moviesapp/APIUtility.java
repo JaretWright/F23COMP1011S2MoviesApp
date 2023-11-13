1c0db6251e96796ab4fe2a07e94c77b3a9f1b181
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-11-13 10:37:56 -0500

    Added the view and controller

--- src/main/java/com/example/f23comp1011s2moviesapp/APIUtility.java
+++ src/main/java/com/example/f23comp1011s2moviesapp/APIUtility.java
@@ -1,5 +1,7 @@
 package com.example.f23comp1011s2moviesapp;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -14,7 +16,10 @@ public class APIUtility {
      * This method will call the OMDB API and save the result in a file called
      * movies.json
      */
-    public static void searchMovies(String movieName) throws IOException, InterruptedException {
+    public static APIResponse searchMovies(String movieName) throws IOException, InterruptedException {
+        //use the replaceAll method to replace a space with %20
+        movieName = movieName.replaceAll(" ","%20");
+
         String uri = "http://www.omdbapi.com/?apikey=4a1010ab&s="+movieName;
 
         //configure the environment to make a HTTP request (this includes an update to
@@ -23,7 +28,17 @@ public class APIUtility {
         HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(uri)).build();
 
         //this will save the json response to a file call movies.json
-        HttpResponse<Path> httpResponse = client.send(httpRequest,
-                                        HttpResponse.BodyHandlers.ofFile(Paths.get("movies.json")));
+//        HttpResponse<Path> httpResponse = client.send(httpRequest,
+//                                        HttpResponse.BodyHandlers.ofFile(Paths.get("movies.json")));
+
+        //this will save the json response to a HttpResponse object
+        HttpResponse<String> response = client.send(httpRequest,
+                                                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+        //Update the pom.xml file for GSON and update module-info.java to work with GSON
+        Gson gson = new Gson();
+        return gson.fromJson(response.body(), APIResponse.class);
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.example.f23comp1011s2moviesapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.net.http;
+    requires com.google.gson;
 
-    opens com.example.f23comp1011s2moviesapp to javafx.fxml;
+    opens com.example.f23comp1011s2moviesapp to javafx.fxml, com.google.gson;
     exports com.example.f23comp1011s2moviesapp;
 }
